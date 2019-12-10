bad80641114f2496395c6df4b93b0c999d89f4f9
Leke.Ibiyode <Leke.Ibiyode@coveainsurance.co.uk>
2019-12-10 14:37:33 +0000

    starting the create fixture tests

--- test/features/RetrieveAllFixtures.feature
+++ test/features/RetrieveAllFixtures.feature
@@ -2,8 +2,8 @@ Feature: Retrieve All Fixtures
 
 Tests Retrieve all Fixtures
 
-Scenario: Retrieve Fixtures
-Given I have called the service
-Then response contains 3 fixtures
-And Each fixture has a fixture id
+# Scenario: Retrieve Fixtures
+# Given I have called the service
+# Then response contains 3 fixtures
+# And Each fixture has a fixture id
 
--- test/steps/StoreNewFixtureSteps.ts
+++ test/steps/StoreNewFixtureSteps.ts
@@ -1,2 +1,44 @@
 import { BeforeAll, Given, Then, When } from "cucumber";
 import fetch from 'node-fetch';
+import * as model from '../models/fixturemodel.json';
+import config from "../utilities/config";
+import { expect } from "chai";
+
+const url = `${config.baseurl}`;
+let serviceresponse:any;
+let fixturesReturned: any;
+let requestBody = model;
+
+Given('I have connected to the resource {string}', async function (resourcepath) {
+    await fetch(`${url}${resourcepath}`,{method:'POST',body:`${requestBody}`})
+    .then(response => response.json())
+    .then(data => {      
+      serviceresponse = data;
+    })
+    .catch((err: any) => console.log(err))
+    });  
+
+
+  When('I ask to create fixture with the data below', function () {
+    // Write code here that turns the phrase above into concrete actions
+    
+  });
+
+
+  Then('fixture is created', function () {
+    // Write code here that turns the phrase above into concrete actions
+    
+  });
+
+
+
+  When('I request the fixture details', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  Then('the first team has Id {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
